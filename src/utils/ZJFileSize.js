095f29faa4df4f433526feb8e8698ad9820b538e
张健 <2603267454@qq.com>
2025-06-03 20:51:59 +0800

    Squashed commit of the following:
    commit 3511aa078e0ffa51e387023f96efaee38bec65ae
    Author: WERTYUZJ <2603267454@qq.com>
    Date:   Tue Jun 3 15:45:11 2025 +0800
    
        优化文档。
    
    commit 810ccaa61a83cb86e4cf5a97b3a793b86ffe39d8
    Author: WERTYUZJ <2603267454@qq.com>
    Date:   Tue Jun 3 14:14:32 2025 +0800
    
        优化部分文档。

--- src/utils/ZJFileSize.js
+++ src/utils/ZJFileSize.js
@@ -1,17 +1,21 @@
-function FileSize(){
+function FileSize() {
   return (fileSize) => {
-    var a =fileSize/1024
-    if(a<1){
-      return (a/1024).toFixed(2) + 'B' 
-    }else if(a<1024 && a>=1){
-      return a.toFixed(2) + 'KB'
-    }else if(a>=1024 && a<1024*1024){
-      return (a/1024).toFixed(2) + 'MB' 
-    }else if(a>=1024*1024){
-      return (a/1024).toFixed(2) + 'GB' 
-    }else{
+    if (!fileSize) {
       return 'fileSizeErr'
     }
+    var a = fileSize / 1024
+    if (a < 1) {
+      return (a / 1024).toFixed(2) + 'B'
+    }
+    if (a < 1024 && a >= 1) {
+      return a.toFixed(2) + 'KB'
+    }
+    if (a >= 1024 && a < 1024 * 1024) {
+      return (a / 1024).toFixed(2) + 'MB'
+    }
+    if (a >= 1024 * 1024) {
+      return (a / 1024).toFixed(2) + 'GB'
+    }
   };
-} 
+}
 export default FileSize;
--- src/views/pinia/Vuex-Pinia/index.vue
+++ src/views/pinia/Vuex-Pinia/index.vue
@@ -8,21 +8,24 @@
       <p>npm install pinia</p>
       <p>1.2创建并配置Pinia实例</p>
       <ZJCodeDisplay :code="vueCode" language="vue" />
-      <p>1.3接下来，创建一个Pinia store来替代Vuex store：</p>
+      <p>1.3接下来，创建一个Pinia store来替代Vuex store：可以下载持久化插件（"pinia-plugin-persistedstate": "^4.3.0"）并配置 persist: {
+        storage: localStorage,}//指定需要的本地存储方式</p>
       <ZJCodeDisplay :code="vueCode2" language="vue" />
-      <p>1.4在你的Vue组件中，你可以这样使用这个store：</p>
+      <p>持久化插件的main.js配置方法如下：</p>
       <ZJCodeDisplay :code="vueCode3" language="vue" />
+      <p>1.4在你的Vue组件中，你可以这样使用这个store：</p>
+      <ZJCodeDisplay :code="vueCode4" language="vue" />
       <p>2.1注意：在Pinia中，state 是一个函数，返回一个对象，这与Vuex的state对象略有不同。
-      在Pinia中，mutations被替换为直接修改state的actions。
-      getters 的使用方式相同，但是在组件中需要通过store实例来访问。</p>
+        在Pinia中，mutations被替换为直接修改state的actions。
+        getters 的使用方式相同，但是在组件中需要通过store实例来访问。</p>
     </ZJMain>
   </div>
 </template>
 
 <script setup>
 
-const vueCode=ref(
-`import { createApp } from 'vue';
+const vueCode = ref(
+  `import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 
@@ -32,47 +35,53 @@ const pinia = createPinia();
 app.use(pinia);
 app.mount('#app');
 `);
-const vueCode2=ref(
-`// stores/userStore.js 或 stores/userStore.ts
-import { defineStore } from 'pinia';
+const vueCode2 = ref(
+  `// src/store/index.js  
+import { defineStore } from "pinia";
+
+// 从 localStorage 读取整个 userData 初始化数据
+const localData = localStorage.getItem('user');
+let userLocalData = localData ? JSON.parse(localData) : {};
 
 export const useUserStore = defineStore('user', {
   state: () => ({
     // 尝试从localStorage加载用户数据，如果没有则默认为空对象
-    userData: (function() {
-      try {
-        const userDataJSON = localStorage.getItem('userData');
-        if (userDataJSON) {
-          const userData = JSON.parse(userDataJSON);
-          // 检查userData是否是一个对象且包含id属性
-          if (typeof userData === 'object' && userData !== null && 'id' in userData) {
-            return userData;
-          }
-        }
-        // 如果userData不存在、不是对象或没有id属性，则返回null
-        return null;
-      } catch (error) {
-        // 如果JSON.parse失败（比如因为格式错误），也返回null
-        console.error('Error parsing userData from localStorage:', error);
-        return null;
-      }
-    })(),
+    userData: {},
+    // 加载动画
     isLoading: false,
+    // 侧边框
+    isSideBarOpen: userLocalData.isSideBarOpen ?? true,
+    // 初始化选中侧边框
+    ZJAsideMenuNameSelect: 'home',
+    // 初始化测边框卡片
+    ZJAsideMenuCardNameSelect: null,
+    // 初始化测边框卡片索引
+    ZJAsideMenuCardNameIndex: -1,
+    // 刷新页面
+    isRouterActive: true,
+    // 布局相关状态
+    layout: {
+      // 头部
+      showHeader: true,
+      // 头部路由
+      showHeaderBar: true,
+      // 头部左侧侧边栏打开按钮
+      showHeaderSildebarOpen: true,
+      // 侧边栏导航
+      showSilderBar: true,
+      // 路由刷新
+      showRouterActive: true,
+      //设置
+      showSet: false,
+      //展示搜索
+      showSearch: false,
+      // 单页面全屏
+      isFullScreenRouter: false,
+      // 是否展示欢迎页面
+      showWelcome: true,
+    },
   }),
   actions: {
-    async setUserData(userData) {
-      this.userData = userData;
-      // 更新状态后，保存到localStorage
-      localStorage.setItem('userData', JSON.stringify(userData));
-    },
-    // 更新用户头像
-    addFieldToUserData(fieldName, fieldValue) {
-      this.userData = {
-        ...this.userData,
-        [fieldName]: fieldValue,
-      };
-      localStorage.setItem('userData', JSON.stringify(this.userData));
-    },
     startLoading() {
       this.isLoading = true;
     },
@@ -82,12 +91,38 @@ export const useUserStore = defineStore('user', {
   },
   getters: {
     getUserData: (state) => state.userData,
-    isLoading: (state) => state.isLoading,
   },
+  // 持久化存储
+  persist: {
+    storage: localStorage,//指定需要的本地存储方式
+  }
+  // persist:{
+  //   storage: sessionStorage,
+  // }
+  // persist:[
+  //   {
+  //     key: 'my-custom-key', // 自定义键名
+  //     storage: sessionStorage,
+  //     paths: ['state.layout.showWelcome'], // 仅持久化 layout.showWelcome
+  //   }
+  // ],
 });
 `);
-const vueCode3=ref(
-`<template>
+const vueCode3 = ref(`
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+const app = createApp(App)
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
+app.mount('#app')
+`)
+const vueCode4 = ref(
+  `<template>
   <div>
     <p v-if="userData">User ID: {{ userData.id }}</p>
     <p v-else>No user data found</p>
