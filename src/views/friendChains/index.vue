f527a651e933f34b802804691b781a347c76c9c6
WERTYUZJ <2603267454@qq.com>
2025-06-05 13:57:59 +0800

    新增friendChains跳转。

--- src/views/friendChains/index.vue
+++ src/views/friendChains/index.vue
@@ -5,7 +5,7 @@
         <h2>宝藏网站</h2>
       </template>
       <div class="main">
-        <div class="main-box" v-for="(i, index) in frendlyChains" :key="index">
+        <div class="main-box" v-for="(i, index) in frendlyChains" :key="index" @click="handelBox(path)">
           <div class="left">
             <img :src="i.img || i.icon" @error="errorImg" alt="icon">
           </div>
@@ -20,11 +20,13 @@
 </template>
 
 <script setup>
+
 const frendlyChains = ref([
   {
     icon: '/ZJUI.png',
     title: "Green Video",
     titleColor: "#36ad6a",
+    path: 'https://greenvideo.cc/',
     content: "GreenVideo的使用是完全免费的，在这里你们能够将视频网址输入此地并下载视频。"
   },
   {
@@ -32,19 +34,25 @@ const frendlyChains = ref([
     img: 'https://sqlpub.com/logo.svg',
     title: "SQLPub",
     titleColor: "#7F56D9",
+    path: 'https://www.sqlpub.com/',
     content: "SQLPub 提供最新版本、甚至是开发者版本的 MySQL 服务器测试服务。"
   },
   {
     icon: '/ZJUI.png',
     img: 'https://tools.pdf24.org/static/img/pageIcons/svg/default.svg?v=5cae54fd',
-    title: "https://tools.pdf24.org/zh/",
+    title: "PDF24",
     titleColor: "#4098fc",
+    path: 'https://tools.pdf24.org/zh/',
     content: "免费且易于使用的在线PDF工具。"
   },
 ])
 
 const errorImg = (event) => {
   event.target.src = '/ZJUI.png'
+}
+
+const handelBox = (path) => {
+  window.open(path, '_blank')
 }
 </script>
 
